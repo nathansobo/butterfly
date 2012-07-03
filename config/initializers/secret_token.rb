75e85508717543b171207c62c6d2b5e31cb37b53
Nathan Sobo & Zef Houssney <nathansobo+zefmail@gmail.com>
2012-07-03 15:25:45 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Butterfly::Application.config.secret_token = '709aa630ef836bb1cbe2104a74f0b5c8940f88eec908ad6b69a8109c9dcddd9f72ff8bd8be5d038547b67e4548a7fae82b95b0d22b9b0b4b7badea598b6508c9'
